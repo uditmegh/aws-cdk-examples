f400c2050175b4d7d0bcaf2dbcd2a407f3d5bb4f
uditmegh <40011600+uditmegh@users.noreply.github.com>
2025-12-01 12:41:44 -0500

    Enable AWS X-Ray tracing for Lambda and API Gateway
    - Add X-Ray active tracing to Lambda function
    - Enable X-Ray tracing on API Gateway stage
    - Grant X-Ray write permissions to Lambda

--- python/apigw-http-api-lambda-dynamodb-python-cdk/lambda/apigw-handler/index.py
+++ python/apigw-http-api-lambda-dynamodb-python-cdk/lambda/apigw-handler/index.py
@@ -6,6 +6,10 @@ import os
 import json
 import logging
 import uuid
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+patch_all()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
